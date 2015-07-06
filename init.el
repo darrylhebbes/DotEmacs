e4d587e49a2214517b1e1851aa7cff049388e07f
Eric James Michael Ritz <ejmr@plutono.com>
2015-07-05 21:55:42 -0400

    [Refactor] Remove custom eproject definition for Python
    Kind of pointless since I'm already using `eproject-python`.

--- init.el
+++ init.el
@@ -730,14 +730,6 @@ _L_ist
     :relevant-files ("\\.php")
     :common-compiles ("composer install"))
 
-  (define-project-type python (generic)
-    (or (look-for "setup.py")
-        (look-for "*.egg-info")
-        (look-for "__init__.py"))
-    :main-file "__init__.py"
-    :relevant-files ("\\.py")
-    :irrelevant-files ("\\.pyc"))
-
   (define-project-type ruby (generic)
     (or (look-for "Gemfile")
         (look-for "*.gemspec")
