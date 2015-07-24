a9e2b37e8b77685ead20dd084251b8710fdac990
Eric James Michael Ritz <ejmr@plutono.com>
2015-07-24 09:28:01 -0400

    [Feature] Create a Hydra for Markdown Mode

--- init.el
+++ init.el
@@ -409,7 +409,42 @@ art_b_ollocks mode                    _r_eplace
   (insert-file-contents "/home/eric/.signature"))
 
 (use-package markdown-mode
-  :config (define-key markdown-mode-map (kbd "C-c C-w") 'ejmr/insert-email-signature))
+  :config
+  (define-key markdown-mode-map (kbd "C-c C-w") 'ejmr/insert-email-signature)
+  (defhydra hydra-markdown (:hint nil)
+    "
+Formatting        C-c C-s    _s_: bold          _e_: italic     _b_: blockquote   _p_: pre-formatted    _c_: code
+
+Headings          C-c C-t    _h_: automatic     _1_: h1         _2_: h2           _3_: h3               _4_: h4
+
+Lists             C-c C-x    _m_: insert item
+
+Demote/Promote    C-c C-x    _l_: promote       _r_: demote     _u_: move up      _d_: move down
+
+Links, footnotes  C-c C-a    _L_: link          _U_: uri        _F_: footnote     _W_: wiki-link      _R_: reference
+
+"
+    ("s" markdown-insert-bold)
+    ("e" markdown-insert-italic)
+    ("b" markdown-insert-blockquote :color blue)
+    ("p" markdown-insert-pre :color blue)
+    ("c" markdown-insert-code)
+    ("h" markdown-insert-header-dwim)
+    ("1" markdown-insert-header-atx-1)
+    ("2" markdown-insert-header-atx-2)
+    ("3" markdown-insert-header-atx-3)
+    ("4" markdown-insert-header-atx-4)
+    ("m" markdown-insert-list-item)
+    ("l" markdown-promote)
+    ("r" markdown-demote)
+    ("d" markdown-move-down)
+    ("u" markdown-move-up)
+    ("L" markdown-insert-link :color blue)
+    ("U" markdown-insert-uri :color blue)
+    ("F" markdown-insert-footnote :color blue)
+    ("W" markdown-insert-wiki-link :color blue)
+    ("R" markdown-insert-reference-link-dwim :color blue))
+  (define-key markdown-mode-map (kbd "C-s-k") #'hydra-markdown/body))
 
 
 (use-package avy
@@ -421,7 +456,7 @@ art_b_ollocks mode                    _r_eplace
     ("l" avy-goto-line "line")
     ("w" avy-goto-word-1 "word")
     ("W" avy-goto-word-0 "word-0")
-    ("s" avy-goto-subword-1 "subword") 
+    ("s" avy-goto-subword-1 "subword")
     ("S" avy-goto-subword-0 "subword-0"))
   (global-set-key (kbd "C-c a") #'hydra-avy/body))
 
